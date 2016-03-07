ff5c383dd35416354b6ff788a948e642a31de770
Zach Weed <zweed123@gmail.com>
2016-03-06 22:23:14 -0500

    splash page

--- Spotlight/urls.py
+++ Spotlight/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # url(r'^$', 'Spotlight.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', TemplateView.as_view(template_name='index.html'),name='home')
+    url(r'^$', TemplateView.as_view(template_name='index.html'),name='home'),
 	url(r'^spotlight/', include('spotlightapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
