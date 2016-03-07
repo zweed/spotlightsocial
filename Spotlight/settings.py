8b742015fcf25e6240aba3d35c20b723f96c6a5d
Zach Weed <zweed123@gmail.com>
2016-03-06 22:28:44 -0500

    splash page

--- Spotlight/settings.py
+++ Spotlight/settings.py
@@ -29,13 +29,13 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
+    'spotlightapp.apps.SpotlightappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'spotlightapp',
 )
 
 MIDDLEWARE_CLASSES = (
