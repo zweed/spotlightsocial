7b3ef8257be2df3fb92d72ad25941ea35368b162
Zach Weed <zweed123@gmail.com>
2016-03-06 21:52:08 -0500

    splash page

--- Spotlight/urls.py
+++ Spotlight/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # url(r'^$', 'Spotlight.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-	url(r'^$', include('spotlightapp.urls')),
+	url(r'^', include('spotlightapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- spotlightapp/views.py
+++ spotlightapp/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+import re
+from urllib2 import Request, urlopen
+from urllib import urlencode
+
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.http import HttpResponse, HttpResponseRedirect
+
+import django.contrib.auth as auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.template import RequestContext, loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	template = loader.get_template('index.html')
+
+	result = True
+	context = RequestContext(request, {
+		'result': result
+		})
+
+    return HttpResponse(template.render(context))
